c8ae44d1e0f34a48cc15a3c1f8b864ed198ee726
zonde306 <13820323+zonde306@users.noreply.github.com>
2022-07-31 17:31:39 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using System.Reflection;
+using System.IO;
+
+namespace RF5_ShopTweak
+{
+	[BepInPlugin(GUID, NAME, VERSION)]
+	[BepInProcess(GAME_PROCESS)]
+	public class Main : BasePlugin
+	{
+		#region PluginInfo
+		private const string GUID = "A465AF6E-9709-261F-EDCA-7E777C4AD8C1";
+		private const string NAME = "RF5_ShopTweak";
+		private const string VERSION = "1.0";
+		private const string GAME_PROCESS = "Rune Factory 5.exe";
+		#endregion
+
+		static public new ManualLogSource Log;
+		private static string FILENAME = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\" + NAME + ".ini";
+		public static new IniParser Config;
+
+		public override void Load()
+		{
+			Log = base.Log;
+			Config = new IniParser(FILENAME);
+			new Harmony(GUID).PatchAll();
+		}
+	}
+}
