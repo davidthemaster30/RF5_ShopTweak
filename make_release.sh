3206cd7fb814c20c154f9b31d9e93c36a7308abe
David <davidthemaster30@hotmail.com>
2025-01-03 00:55:00 -0500

    Add ini file

--- make_release.sh
+++ make_release.sh
@@ -3,4 +3,4 @@ rm -rf bin
 
 dotnet build RF5_ShopTweak.csproj -f net6.0 -c Release
 
-zip -j 'RF5_ShopTweak_v1.3.0.zip' './bin/Release/net6.0/RF5_ShopTweak.dll'
+zip -j 'RF5_ShopTweak_v1.3.0.zip' './bin/Release/net6.0/RF5_ShopTweak.dll' './bin/Release/net6.0/RF5_ShopTweak.ini'
