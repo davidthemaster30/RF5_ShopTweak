0873fa683809accda1b795accf4e6c8bafcd6a03
David Tremblay <davidthemaster30@hotmail.com>
2026-08-25 23:31:37 -0400

    Add processed state, else the pages get added twice

--- ShopDataTableHandler.cs
+++ ShopDataTableHandler.cs
@@ -6,13 +6,21 @@ namespace RF5_ShopTweak;
 [HarmonyPatch]
 internal static class ShopDataTableHandler
 {
-	[HarmonyPatch(typeof(UIShopController), nameof(UIShopController.SetShopTable))]
-	[HarmonyPostfix]
+	private static bool _processed = false;
+
+    [HarmonyPatch(typeof(UIShopController), "SetShopTable")]
+    [HarmonyPostfix]
 	internal static void UIShopControllerTweaks(UIShopController __instance)
 	{
 		ShopTweakPlugin.Log.LogDebug($"UIShopController.SetShopTable");
-		if (__instance is null || __instance.ShopMenuType != ShopMenuType.ITEM)
+		if (__instance is null || __instance.ShopMenuType != ShopMenuType.ITEM || __instance.NpcShopTable == null)
 		{
+			return;
+		}
+
+        if (_processed)
+		{
+			ShopTweakPlugin.Log.LogDebug($"ApplyShopTweaks Skipping already processed shop:{__instance.NpcShopTable.name}");
 			return;
 		}
 
@@ -20,6 +28,7 @@ internal static class ShopDataTableHandler
 
 		var shop = ShopTweakPlugin.Shops.FirstOrDefault(x => x.ShopType == __instance.shopType);
 		shop?.ShopTweaks.ApplyAll(__instance.NpcShopTable);
+		_processed = true;
 
 		ShopTweakPlugin.Log.LogDebug($"ApplyShopTweaks End shopType:{__instance.shopType}, shopPages:{__instance.NpcShopTable.ShopCatalogPages.Count}");
 
@@ -36,8 +45,16 @@ internal static class ShopDataTableHandler
 			__instance.UIShopControl.discountRate = shop.PriceMultiplier;
 			ShopTweakPlugin.Log.LogDebug($"ShopDataTableHandler applied discountRate:{shop.PriceMultiplier}");
 		}
-		ShopTweakPlugin.Log.LogDebug($"ShopDataTableHandler currentShopType:{__instance.shopType}");
 	}
+
+	[HarmonyPatch(typeof(UIShopController), "CloseShop")]
+    [HarmonyPostfix]
+	internal static void UIShopControllerTweaksCleanup(UIShopController __instance)
+	{
+		ShopTweakPlugin.Log.LogDebug($"UIShopController.CloseShop");
+		_processed = false;
+	}
+
 }
 
 
